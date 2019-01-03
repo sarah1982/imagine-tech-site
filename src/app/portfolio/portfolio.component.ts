6706bed7aca326f20a36ad22d723a73336bdeac0
Vimal Gopalakrishnan <vimalgopalakrishnan@Vimals-Mac-mini-8.local>
2019-01-03 15:46:26 -0800

    modified th econtainer

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-
+ img;
+ modalImg;
+captionText;
+ modal;
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
+  ngOnViewInit() {
+     this.modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+ this.img = document.getElementById('myImg');
+ this.modalImg = document.getElementById('img01');
+ this.captionText = document.getElementById('caption');
+  }
+  getImg() {
+    this.modal.style.display = 'block';
+    this.modalImg.src = this.img.src;
+    this.captionText.innerHTML = this.img.alt;
+  }
+  closeImg() {
+    this.modal.style.display = 'none';
+  }
 }
